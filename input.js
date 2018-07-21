2171d4f51d83ef93b10bfe5e7262c3cb1eec0b26
Pavel V <vorona.pavel.nickolaevich@gmail.com>
2018-07-21 22:36:22 +0300

    Move js to separate file

---
+++ input.js
@@ -0,0 +1,27 @@
+class PvInput extends HTMLElement {
+  connectedCallback () {
+    this.innerHTML = `
+    <div class="c-input">
+      <div class="container">
+        <input
+          class="pv-input"
+          placeholder="${this.getAttribute('placeholder')}"
+          spellcheck="false"
+          autocomplete="off"
+          autocorrect="off"
+          autocapitalize="off"
+          autofocus="${this.getAttribute('autofocus') || false}"
+        >
+        <div class="pv-input__label">${this.getAttribute('label')}</div>
+        <div class="pv-input__hint">${this.getAttribute('hint')}</div>
+        <div class="pv-input__hint error">${this.getAttribute('error')}</div>
+        <div class="pv-input__hint success">${this.getAttribute('success')}</div>
+        <div class="pv-input__overflow"></div>
+        <div class="pv-input__accent-line"></div>
+      </div>
+    </div>
+    `
+  }
+}
+
+customElements.define('pv-input', PvInput)
