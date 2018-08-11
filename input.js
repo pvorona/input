62d9103abccad036e376934b36447725e30b0911
Pavel V <vorona.pavel.nickolaevich@gmail.com>
2018-08-11 15:35:14 +0300

    Remove old notes

--- input.js
+++ input.js
@@ -1,25 +1,25 @@
 class PvInput extends HTMLElement {
   connectedCallback () {
     this.innerHTML = `
-    <div class="c-input">
-      <div class="container">
-        <input
-          class="pv-input"
-          placeholder="${this.getAttribute('placeholder')}"
-          spellcheck="false"
-          autocomplete="off"
-          autocorrect="off"
-          autocapitalize="off"
-          ${this.getAttribute('autofocus') ? 'autofocus' : ''}
-        >
-        <div class="pv-input__label">${this.getAttribute('label')}</div>
-        <div class="pv-input__hint">${this.getAttribute('hint')}</div>
-        <div class="pv-input__hint error">${this.getAttribute('error')}</div>
-        <div class="pv-input__hint success">${this.getAttribute('success')}</div>
-        <div class="pv-input__overflow"></div>
-        <div class="pv-input__accent-line"></div>
+      <div class="c-input">
+        <div class="container">
+          <input
+            class="pv-input"
+            placeholder="${this.getAttribute('placeholder')}"
+            spellcheck="false"
+            autocomplete="off"
+            autocorrect="off"
+            autocapitalize="off"
+            ${this.getAttribute('autofocus') ? 'autofocus' : ''}
+          >
+          <div class="pv-input__label">${this.getAttribute('label')}</div>
+          <div class="pv-input__hint">${this.getAttribute('hint')}</div>
+          <div class="pv-input__hint error">${this.getAttribute('error')}</div>
+          <div class="pv-input__hint success">${this.getAttribute('success')}</div>
+          <div class="pv-input__overflow"></div>
+          <div class="pv-input__accent-line"></div>
+        </div>
       </div>
-    </div>
     `
   }
 }
